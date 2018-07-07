bf892af337cb259bc83f319f50276514c122a1e8
kushenchirekova <31691091+kushenchirekova@users.noreply.github.com>
2018-07-08 00:13:45 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+token = "587546791:AAE0wt8S2yIj-7lLX7D0LHN_d0k-42kRHr4"
+
+db_name = "lol_base.db"
+users_table = "users"
+estabs_table = "estabs"
+
+
+# Buttons for users
+add = 'Добавить заведение'
+choose = 'Выбрать заведение'
+find = "Найти couchsurfer'а"
+tobe = "Стать couchsurfer'ом"
+neighbour = "Найти людей рядом"
+
+# Buttons for categories
+cat1 = 'Развлечения'
+cat2 = 'Ресторан'
+cat3 = 'Достопримечательность'
+cat4 = 'Отель'
+
+# Buttons for prices
+p1 = 'До 1000'
+p2 = '1000-2000'
+p3 = '2000-3500'
+p4 = '3500-5000'
+p5 = '5000-7000'
+p6 = '7000-10000'
+p7 = '10000-12000'
+p8 = '12000-14000'
+p9 = '14000-16000'
